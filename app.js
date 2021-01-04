cbf20fa5b8d237a005c5b195ba4554e37fd949bb
Dragos Ciopeica <dragos.ciopeica@acrux.biz>
2021-01-04 21:09:45 +0200

    Lectia 10, numere si Math operator

---
+++ app.js
@@ -0,0 +1,103 @@
+
+// // console.log(123);
+
+// // console.clear();
+
+
+// // console.warn('this is a warning');
+
+// // console.time('hello')
+// // console.log('Dragos a fost aici!');
+// // console.log('Dragos a fost aici!');
+// // console.log('Dragos a fost aici!');
+// // console.log('Dragos a fost aici!');
+// // console.log('Dragos a fost aici!');
+// // console.log('Dragos a fost aici!');
+// // console.timeEnd('hello')
+
+
+//=====================================> Var let si cons <=====================================
+// // var, let, const
+
+// //  var nume = 'John Doe';
+// //  console.log(nume);
+
+// //  nume = 'steve smith';
+// //  console.log(nume);
+
+// //  // Init var
+
+// //  var greeting;
+// //  console.log(greeting);
+
+// //  greeting = 'Hello';
+// //  console.log(greeting);
+
+//  // letters, numbers, _, $
+//  // Can not start with NUMBER !!! 
+
+// //  var 1name= 'john';
+
+
+//=====================================> Exemplu transformare din NR in String si invers <=====================================
+
+// let val;
+
+// val = String(555);
+// val = String(4+4);
+// //bool to string
+// val = String(true);
+
+// //date to string
+// val = String(new Date());
+
+// //array to string
+// val = String([1,2,3,4]);
+
+// // Number vs Parsefloat
+
+// val = Number('100.30');
+// val = parseFloat('100.30');
+
+// //Output 
+// console.log(val);
+// console.log(typeof val);
+// // console.log(val.length); // Only for strings
+// console.log(val.toFixed(2)); // only for numbers
+
+
+//=====================================> Numere si Math operator <=====================================
+
+const num1= 100;
+const num2= 60;
+let val;
+
+val = num1 + num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object
+
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(9, 3);
+val = Math.min(9,3,2,-1,2,32,4,24,1,23,1,31,3);
+val = Math.max(9,3,2,-1,2,32,4,24,1,23,1,31,3);
+val = Math.random();
+
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
+
+
+
+
+
+
+
